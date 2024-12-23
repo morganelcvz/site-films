e0d5982eb558d902ffa68aa6c69663b1378db51d
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2024-12-23 15:48:28 +0100

    ajout exercice json

---
+++ js/script.js
@@ -0,0 +1,24 @@
+fetch("js/movies.json")
+    .then(response => response.json())
+    .then(data => {showdata(data)})
+    .catch(error => console.error(error));
+
+    function showdata(data) {
+    const results = data.results
+
+    results.forEach(film => {
+        document.getElementById(myMovies).innerHTML = `
+            <div class="movie-card">
+                    <div class="title-movie">${film.title}</div>
+                    <img src="https://image.tmdb.org/t/p/original${film.poster_path}">
+                <div class="head-movie">
+                    <div class="note"><i class="fa-regular fa-star"></i><span>${film.vote_average}</span></div>
+                    <div class="date-movie">${film.release_date}</div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+
+
