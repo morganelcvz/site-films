c2874ab76f86e21af6252d314470a65d4a099518
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-02 16:53:17 +0100

    ajout site terminé correction date FR

--- js/script.js
+++ js/script.js
@@ -7,10 +7,13 @@ fetch("js/movies.json")
     const results = data.results
 
     results.forEach(film => {
+        const release = new Date(film.release_date)
+
+        const datefr = release.toLocaleDateString("fr")
         document.getElementById("myMovies").innerHTML += `
             <div class="movie-card">
                     <img src="https://image.tmdb.org/t/p/original${film.poster_path}">
-                    <div class="date-movie">${film.release_date}</div>
+                    <div class="date-movie">${datefr}</div>
                 <div class="head-movie">
                     <div class="note"><i class="fa-regular fa-star"></i><span>${film.vote_average}</span></div>
                     <div class="title-movie">${film.title}</div>
@@ -19,3 +22,4 @@ fetch("js/movies.json")
         `;
     });
 }
+
