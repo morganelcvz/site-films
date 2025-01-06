74d090ccf6ac5808d1e06b5da987d5278457abff
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2025-01-06 15:36:48 +0100

    ajout one page corrigé

--- js/newscript.js
+++ js/newscript.js
@@ -1,46 +1,106 @@
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTRkOGQ5ODkxNjdlZjBjODE5MDkwZDA4NTg2MjBlOCIsIm5iZiI6MTczNTgwNDYxMC44NDYwMDAyLCJzdWIiOiI2Nzc2NDZjMjViOGY0MjJiNjAxMmUxNWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.IiBc4taQH9FDBM5PuexHYruPI7a-OJ1s3BIjaR3fK9I'
+//     }
+//   };
+  
+//   fetch('https://api.themoviedb.org/3/movie/movie_id?language=fr-FR', options)
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+
 fetch("js/details.json")
     .then(response => response.json())
-    .then(data => {showdata(data)})
-    .catch(error => console.error(error));
+    .then(data => {
+        showdata(data) })
+    .catch(error => console.error(error))
 
-    function showdata(data) {
-    const results = data.results
 
-    results.forEach(one => {
-        // const release = new Date(film.release_date)
+function showdata(data) {
+ 
+    fetch("js/credits.json")
+    .then(response => response.json())
+    .then(credit => {
+        const memberName = credit.crew.filter(member => member.job == "Writer").map(member => member.name)
 
-        // const datefr = release.toLocaleDateString("fr")
+        const release = new Date(data.release_date)
 
+        const datefr = release.toLocaleDateString("fr")
+    
+        const genrelist = data.genres.map(genre => genre.name).join(', ') 
+    
         document.getElementById("oneMovie").innerHTML = `
-        <img src="https://image.tmdb.org/t/p/original/${one.poster_path}">
-        <div class="film-right">
-            <div class="film-title">
-                <h1><b>${one.original_title}</b> <span>(1999)</span></h1>
-            </div>
-            <div class="film-info">
-                <div class="f3">
-                    <div class="f-info">
-                        <p class="genre">${one.genres}</p>
-                        <p class="date">23/06/1999</p>
-                    </div>
-                    
-                    <div class="f-staff">
-                        <span>Nom prénom</span>
-                        <p>Réalisateur</p>
-                    </div>
+            <img src="https://image.tmdb.org/t/p/original/${data.poster_path}">
+            <div class="film-right">
+                <div class="film-title">
+                    <h1><b>${data.original_title}</b> <span>(1999)</span></h1>
+                </div>
+                <div class="film-info">
+                    <div class="f3">
+                                    <div class="note"><i class="fa-regular fa-star"></i><span>${data.vote_average}</span></div>
+                        <div class="f-info">
+                            <p class="genre">${genrelist}</p>
+                            <p class="date">${datefr}</p>
+                        </div>
 
-                    <div class="f-staff">
-                        <span>Nom prénom</span>
-                        <p>Scénariste</p>
+                        ${createWriters(memberName)}
+                        
+                    </div>
+    
+                    <div class="resume">
+                        <h2>Synopsis</h2>
+                        <p>${data.overview}</p>
                     </div>
                 </div>
-                <div class="note"><i class="fa-regular fa-star"></i><span>${one.vote_average}</span></div>
-                <div class="resume">
-                    <h2>Synopsis</h2>
-                    <p>${one.overview}</p>
+            </div>
+            `
+
+            document.getElementById("castId").innerHTML = `
+            ${createCast(credit.cast)} 
+            `
+    })
+
+}
+
+function createWriters (writers){
+    let myDiv = ''
+    writers.forEach(element => {
+        myDiv += `
+                <div class="f-staff">
+                        <span>${element}</span>
+                        <p>Writer</p>
+                </div>
+        `
+    });
+
+    return myDiv
+}
+
+
+function createCast (tableau){
+    let myDiv = ''
+    let count = 10 
+    tableau.forEach(element => {
+        count--
+        if (count >= 0){
+            myDiv  += `
+            <div class="actors">
+                <div class="actors-profile">
+                     <img src="https://image.tmdb.org/t/p/original/${element.profile_path}">
+                     <br/>
+                     <span>${element.name}</span>
+                     <p>${element.character}</p>
                 </div>
             </div>
-        </div>
-        `;
-    });
+    `
+        }
+
+    })
+
+    return myDiv
 }
+
+const url = new URLSearchParams(window.location.search);
+const id= url.get('id')
--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-fetch("js/movies.json")
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTRkOGQ5ODkxNjdlZjBjODE5MDkwZDA4NTg2MjBlOCIsIm5iZiI6MTczNTgwNDYxMC44NDYwMDAyLCJzdWIiOiI2Nzc2NDZjMjViOGY0MjJiNjAxMmUxNWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.IiBc4taQH9FDBM5PuexHYruPI7a-OJ1s3BIjaR3fK9I'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1&region=FR', options)
     .then(response => response.json())
     .then(data => {showdata(data)})
-    .catch(error => console.error(error));
+    .catch(error => console.error(error))
 
     function showdata(data) {
     const results = data.results
@@ -11,6 +19,7 @@ fetch("js/movies.json")
 
         const datefr = release.toLocaleDateString("fr")
         document.getElementById("myMovies").innerHTML += `
+        <a href="movies.html?id=${film.id}">
             <div class="movie-card">
                     <img src="https://image.tmdb.org/t/p/original${film.poster_path}">
                     <div class="date-movie">${datefr}</div>
@@ -19,7 +28,11 @@ fetch("js/movies.json")
                     <div class="title-movie">${film.title}</div>
                 </div>
             </div>
+            </a>
         `;
     });
 }
+
+const url = new URLSearchParams(window.location.search);
+const id= url.get('id')
 
